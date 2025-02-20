4c771319739f36dd54f3bccb9dcd5d9e7d2809f6
Tabbu <162439974+tabbukhatoon@users.noreply.github.com>
2025-02-20 05:27:25 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,26 @@
+/*
+loop
+while 
+do while 
+for of loop
+for in loop 
+string length
+string index
+string literals
+interpolation
+scap character
+tap character
+str.touppercase
+st.tolowercase
+str.trim
+str.slide
+str.concate
+str.replace
+str.charat
+*/
+let i=1;
+while(i<=100){
+    console.log("i=",i);
+i++;
+}
+// alert("")
